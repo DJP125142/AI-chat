33d4d2fedb121410fd5382eab90d21f29240bdb6
Jasper <929435979@qq.com>
2024-04-07 02:32:23 +0800

    env

--- services/init.py
+++ services/init.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
 import os
+
+# 加载.env文件
+load_dotenv()
 
 def init():
     # 在环境变量中配置openai的key和代理地址
-    os.environ["OPENAI_API_KEY"] = ""
-    os.environ["OPENAI_API_BASE"] = "https://3.5.996444.icu/v1"
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    os.environ["OPENAI_API_BASE"] = os.getenv("OPENAI_API_BASE")
